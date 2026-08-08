2906bbe35bc7ff75c933665be8a5e014b0ddaeb8
Oscar86tw <127740420+Oscar86tw@users.noreply.github.com>
2026-08-08 21:57:33 +0800

    UTOP-V5.1.3.27_完整檢查與版本快取同步修正 -1

--- config/app-config.js
+++ config/app-config.js
@@ -1,8 +1,8 @@
 export const APP_CONFIG = Object.freeze({
   appName: '昱拓弱電有限公司 UTOP-3D',
-  version: '5.1.3.26',
+  version: '5.1.3.27',
   defaultQuality: 'ultra',
-  revision: 'V51326_MODULAR_ISOLATION_BLUEPRINT',
+  revision: 'V51327_VERSION_CACHE_SYNC',
   baseUiVersion: '1.1.4 Professional UI',
   spreadsheetId: '1lmEqbYals_uBIOrQFDupYR2hs0iks5zEJM6FTOuOwP0',
   configSheetName: '工作表1',
